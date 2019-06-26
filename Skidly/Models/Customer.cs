1a21d6459b96e968c03a71bcfd21710a5eb8affd
Andy <Andyclem84@hotmail.com>
2019-06-26 19:58:18 +0100

    Added MembershipType model and customer navigational property

--- Skidly/Models/Customer.cs
+++ Skidly/Models/Customer.cs
@@ -9,5 +9,7 @@ namespace Skidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
     }
 }
