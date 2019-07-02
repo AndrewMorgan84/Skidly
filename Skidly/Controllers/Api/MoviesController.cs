4607dd0916be4aea30837858c9beb350b960fe96
Andy <Andyclem84@hotmail.com>
2019-07-02 18:08:40 +0100

    Added Movie API Controller and Customer and Movie DTOs

---
+++ Skidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,84 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Skidly.Models;
+
+namespace Skidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MoviesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET /api/movies
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _dbContext.Movies.ToList();
+        }
+
+        // GET /api/movies/1
+        [HttpGet]
+        public Movie GetMovie(int id)
+        {
+            var movie = _dbContext.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return movie;
+        }
+
+        // POST /api/movie
+        [HttpPost]
+        public Movie CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
+
+            return movie;
+        }
+
+        // PUT /api/movie/1
+        [HttpPut]
+        public void UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var movieInDb = _dbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            movieInDb.Name = movie.Name;
+            movieInDb.GenreId = movie.GenreId;
+            movieInDb.DateAdded = movie.DateAdded;
+            movieInDb.NumberInStock = movie.NumberInStock;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+
+            _dbContext.SaveChanges();
+
+        }
+
+        // DELETE /api/movie/1
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+            var movieInDb = _dbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _dbContext.Movies.Remove(movieInDb);
+            _dbContext.SaveChanges();
+        }
+    }
+}
